504c0e41b806958c1b403880a92501b5cdaefcf1
Ismail Ali <ismailali07863@gmail.com>
2023-11-30 12:08:49 +0530

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,33 @@
+// alert("Hello");
+// console.log("Hello");
+// let age = 21; //overwritten later
+// const name = "Ismail"//not overwritten
+
+// console.log(age);
+
+// String
+
+let firstName = "Ismail";
+let lastName = "Ali";
+let random = "Learning JS";
+
+// Access String character
+// console.log(firstName[0]);
+
+// String Methods
+
+// console.log(firstName.toUpperCase());
+// console.log(lastName.toLowerCase());
+
+// for piece of characters in strings👇
+// console.log(random.substring(0, 8));
+// console.log(random.slice(0, 8));
+
+// Case Sensitive String Methods 👇
+// console.log(firstName.indexOf("l"));
+// console.log(firstName.lastIndexOf("i"));
+// console.log(lastName.replace("Ali", "Khan"));
+// console.log(lastName.replaceAll("i", "-"));
+// console.log(firstName.startsWith("I"));
+// console.log(firstName.endsWith("l"));
+// console.log(random.includes("JS"));
